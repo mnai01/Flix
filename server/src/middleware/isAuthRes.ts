ce8817e7091969c4393a93803b6377c9df9c3457
Ian M <44310933+mnai01@users.noreply.github.com>
2022-02-08 23:53:53 -0500

    add express middleware auth and genre routes

--- server/src/middleware/isAuthRes.ts
+++ server/src/middleware/isAuthRes.ts
@@ -1,10 +1,9 @@
-/* eslint-disable no-undef */
 import { verify } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
 
 // We expect the user to send a header called authorization of the format where it says bearer in front following a spaae then a token
 // EX. bearer isdfj9302hf398h2
-export const isAuthRes = ({ res, req }: any) => {
-    console.log(req.headers['authorization'], 'AUTH');
+export const isAuthRes = (req: Request, res: Response, next: NextFunction) => {
     const authorization = req.headers['authorization'];
 
     // Read header Authorization
@@ -22,9 +21,10 @@ export const isAuthRes = ({ res, req }: any) => {
         const payload = verify(token, process.env.ACCESS_TOKEN_SECRET!);
 
         // Add payload to context so the resolver using isAuth has access to the verified userID information
-        req.payload = payload as any;
+        res = payload as any;
     } catch (err) {
         console.log(err);
         throw new Error('Not Authenticated');
     }
+    next();
 };
--- server/src/routes/movies.ts
+++ server/src/routes/movies.ts
@@ -1,5 +1,8 @@
 import express from 'express';
-import { isAuthRes } from 'src/middleware/isAuthRes';
+import { getGenres } from '../controllers/movieController';
+import { isAuthRes } from '../middleware/isAuthRes';
 const router = express.Router();
 // add controller
-router.get('/', isAuthRes);
+router.get('/genres', isAuthRes, getGenres);
+
+export default router;
