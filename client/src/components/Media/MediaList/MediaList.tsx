3c25bad294cd838035760a583a7bcf05ffe15eaf
Ian M <44310933+mnai01@users.noreply.github.com>
2022-02-15 23:52:10 -0500

    Add swiper example

--- client/src/components/Media/MediaList/MediaList.tsx
+++ client/src/components/Media/MediaList/MediaList.tsx
@@ -1,6 +1,10 @@
 import { Box, Flex, Skeleton } from '@chakra-ui/react';
+import SwiperCore, { Autoplay, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { MediaCard } from '../';
 import { SearchVideos_SearchVideos } from '../../../apollo/generated/SearchVideos';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
 
 interface MediaListProps {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -8,28 +12,35 @@ interface MediaListProps {
     loading?: boolean;
     horizontal?: boolean;
 }
+SwiperCore.use([Navigation, Pagination]);
 
 const MediaList: React.FC<MediaListProps> = ({ medias, loading, horizontal = true }) => {
     return (
-        <Flex wrap={horizontal ? 'nowrap' : 'wrap'} overflowX={'scroll'}>
-            {loading ? (
-                Array(36)
-                    .fill(0)
-                    .map((_, i) => (
-                        <Box key={i} p={1.5}>
-                            <Skeleton width='185px' height='278px' />
-                        </Box>
+        <div>
+            <Swiper grabCursor={true} centeredSlides={true} slidesPerView={8} pagination={false}>
+                {loading ? (
+                    Array(36)
+                        .fill(0)
+                        .map((_, i) => (
+                            <SwiperSlide key={i}>
+                                <Box p={1.5}>
+                                    <Skeleton width="185px" height="278px" />
+                                </Box>
+                            </SwiperSlide>
+                        ))
+                ) : medias && medias.length > 0 ? (
+                    medias.map((i: SearchVideos_SearchVideos) => (
+                        <SwiperSlide key={i.id}>
+                            <Box p={1.5}>
+                                <MediaCard media={i} />
+                            </Box>
+                        </SwiperSlide>
                     ))
-            ) : medias && medias.length > 0 ? (
-                medias.map((i: SearchVideos_SearchVideos) => (
-                    <Box key={i.id} p={1.5}>
-                        <MediaCard media={i} />
-                    </Box>
-                ))
-            ) : (
-                <h1>No result found</h1>
-            )}
-        </Flex>
+                ) : (
+                    <h1>No result found</h1>
+                )}
+            </Swiper>
+        </div>
     );
 };
 
