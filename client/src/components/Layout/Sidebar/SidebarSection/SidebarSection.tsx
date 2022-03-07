b1e5346e0a83b8009669cfc2589e5df076b605d8
Ian M <44310933+mnai01@users.noreply.github.com>
2022-03-07 00:51:05 -0500

    Removed home page categories

--- client/src/components/Layout/Sidebar/SidebarSection/SidebarSection.tsx
+++ client/src/components/Layout/Sidebar/SidebarSection/SidebarSection.tsx
@@ -4,25 +4,33 @@ import { MenuItemProps } from '../Sidebar';
 
 interface SidebarSectionProps {
     title: string;
-    items?: MenuItemProps[];
+    items?: MenuItemProps[] | false;
+    loading?: boolean;
 }
 
-const SidebarSection: React.FC<SidebarSectionProps> = ({ title, items }) => {
+const SidebarSection: React.FC<SidebarSectionProps> = ({ title, items, loading }) => {
     return (
         <>
-            <Divider orientation='horizontal' />
-            <Text fontSize={'sm'} as='b' p={'16px'} textTransform={'uppercase'}>
+            <Divider orientation="horizontal" />
+            <Text fontSize={'sm'} as="b" p={'16px'} textTransform={'uppercase'}>
                 {title}
             </Text>
-            {items
-                ? items.map((i: MenuItemProps) => <NavButton key={i.label} path={i.path} label={i.label} leftIcon={i.leftIcon} isFullWidth />)
-                : Array(19)
-                      .fill(0)
-                      .map((_, i) => (
-                          <Box key={i} p={1.5}>
-                              <Skeleton width='100%' height='30px' />
-                          </Box>
-                      ))}
+
+            {loading ? (
+                Array(3)
+                    .fill(0)
+                    .map((_, i) => (
+                        <Box key={i} p={1.5}>
+                            <Skeleton width="100%" height="30px" />
+                        </Box>
+                    ))
+            ) : !items ? (
+                <Box px={4} pb={4}>
+                    <Text as="i" color={'gray.500'}>{`Select "Movies" or "TV"`}</Text>
+                </Box>
+            ) : (
+                items.map((i: MenuItemProps) => <NavButton key={i.label} path={i.path} label={i.label} leftIcon={i.leftIcon} isFullWidth />)
+            )}
         </>
     );
 };
