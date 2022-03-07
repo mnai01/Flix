821fd15f188fe3896aac85a2292b41ab2bbe37fb
Ian M <44310933+mnai01@users.noreply.github.com>
2022-03-07 00:51:05 -0500

    Removed home page categories

--- client/src/components/Layout/Sidebar/Sidebar.tsx
+++ client/src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,7 +1,6 @@
 import { Flex } from '@chakra-ui/react';
 import { FaDesktop, FaFilm, FaHome } from 'react-icons/fa';
 import SidebarSection from './SidebarSection';
-import { uncapitalize } from '../../../utils/helper/FirstCharacterHelper';
 import useGenreParams from '../../../utils/hooks/useGenreParams';
 import AutoSuggestion from '../../AutoSuggestion';
 import ModeSwitch from '../../ModeSwitch';
@@ -35,20 +34,14 @@ const NavigationMenu: MenuItemProps[] = [
 ];
 
 const Sidebar = () => {
-    const { data, type } = useGenreParams();
-
-    const genreArr: MenuItemProps[] | undefined = data[type]?.map((i: any) => {
-        return { path: `/${type}/category/${uncapitalize(i.name)}`, label: i.name, isFullWidth: true };
-    });
-
-    console.log(genreArr);
+    const { data, type, loading } = useGenreParams();
 
     return (
         // other sites use 240px
         <Flex direction="column" flexBasis={'220px'} flexGrow={0} flexShrink={0} height="100%" justify="space-around" p={1}>
             <AutoSuggestion />
             <SidebarSection title={'Navigation'} items={NavigationMenu} />
-            <SidebarSection title={type === 'tv' ? 'TV Categories' : type === 'movies' ? 'Movie Categories' : 'Categories'} items={genreArr} />
+            <SidebarSection title={type === 'tv' ? 'TV Categories' : type === 'movies' ? 'Movie Categories' : 'Categories'} items={data} loading={loading} />
             <ModeSwitch />
         </Flex>
     );
--- client/src/utils/hooks/useGenreParams.tsx
+++ client/src/utils/hooks/useGenreParams.tsx
@@ -4,6 +4,7 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Genres, Genres_Genres } from '../../apollo/generated/Genres';
 import { GET_GENRES } from '../../apollo/queries';
 import { useGenres } from '../../components/Providers/GenreProvider';
+import { uncapitalize } from '../helper/FirstCharacterHelper';
 
 const useGenreParams = () => {
     const { data, loading, error } = useGenres();
@@ -11,23 +12,30 @@ const useGenreParams = () => {
     const navigate = useNavigate();
     const { pathname } = useLocation();
     const temp = pathname.split('/')[1] as 'tv' | 'movies' | 'home';
-    const type: 'tv' | 'movies' | 'home' = temp === 'tv' || temp === 'movies' ? temp : 'home';
+    const type: 'tv' | 'movies' | false = temp === 'tv' || temp === 'movies' ? temp : false;
     const { genre } = useParams() as { genre: string };
     const [genreSelected, setGenreSelected] = useState<any>();
 
-    const customCategory = { home: [{ name: 'Top Trending', id: '9999999' }] };
-    const customData = { ...data?.Genres, ...customCategory };
-
     useEffect(() => {
-        if (genre && !loading && type && customData[type]?.find((i) => i.name === genre.replace(/\b\w/g, (c) => c.toUpperCase()))) {
-            setGenreSelected(customData[type]?.find((i) => i.name === genre.replace(/\b\w/g, (c) => c.toUpperCase())));
+        if (genre && !loading && type && data?.Genres[type]?.find((i) => i.name === genre.replace(/\b\w/g, (c) => c.toUpperCase()))) {
+            setGenreSelected(data?.Genres[type]?.find((i) => i.name === genre.replace(/\b\w/g, (c) => c.toUpperCase())));
         }
-        if (genre && !loading && type && !customData[type]?.find((i) => i.name === genre.replace(/\b\w/g, (c) => c.toUpperCase()))) {
+        if (genre && !loading && type && !data?.Genres[type]?.find((i) => i.name === genre.replace(/\b\w/g, (c) => c.toUpperCase()))) {
             navigate('/NotFound');
         }
     }, [genre, loading, data, type]);
 
-    return { genre: genreSelected, loading, error, data: customData, type };
+    return {
+        genre: genreSelected,
+        loading,
+        error,
+        data: type
+            ? data?.Genres[type]?.map((i: any) => {
+                  return { path: `/${type}/category/${uncapitalize(i.name)}`, label: i.name, isFullWidth: true };
+              })
+            : (false as const),
+        type,
+    };
 };
 
 export default useGenreParams;
