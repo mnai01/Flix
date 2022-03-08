06ced82093162870eede616d2f14db587767d429
Ian M <44310933+mnai01@users.noreply.github.com>
2022-03-08 04:38:19 -0500

    Update ModeSwitch

--- client/src/components/ModeSwitch/ModeSwitch.tsx
+++ client/src/components/ModeSwitch/ModeSwitch.tsx
@@ -1,14 +1,16 @@
-import { Flex, Switch, Text, useColorMode } from '@chakra-ui/react';
+import { Box, Flex, Switch, Text, useColorMode } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 const ModeSwitch = () => {
     const { colorMode, toggleColorMode } = useColorMode();
 
     return (
-        <Flex align={['center']} gap={5}>
-            <Text fontSize='sm' as='b'>
+        <Flex align={['center']} gap={3}>
+            <Box my={'auto'}>{colorMode === 'light' ? <FaMoon color="grey" /> : <FaSun color="orange" />}</Box>
+            <Text fontSize="sm" as="b">
                 Toggle {colorMode === 'dark' ? 'light' : 'dark'}
             </Text>
-            <Switch size='md' onChange={toggleColorMode} defaultChecked />
+            <Switch size="md" onChange={toggleColorMode} defaultChecked />
         </Flex>
     );
 };
