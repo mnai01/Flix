c5e5b45c33c541d41da23dc35de4823d9a75a056
Ian M <44310933+mnai01@users.noreply.github.com>
2022-03-08 04:38:19 -0500

    Update ModeSwitch

--- client/src/components/Layout/Sidebar/Sidebar.tsx
+++ client/src/components/Layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { FaDesktop, FaFilm, FaHome } from 'react-icons/fa';
 import SidebarSection from './SidebarSection';
 import useGenreParams from '../../../utils/hooks/useGenreParams';
@@ -40,9 +40,12 @@ const Sidebar = () => {
         // other sites use 240px
         <Flex direction="column" flexBasis={'220px'} flexGrow={0} flexShrink={0} height="fit-content" justify="space-around" p={1}>
             <AutoSuggestion />
+            <Box p={3}>
+                <ModeSwitch />
+            </Box>
+
             <SidebarSection title={'Navigation'} items={NavigationMenu} />
             <SidebarSection title={type === 'tv' ? 'TV Categories' : type === 'movies' ? 'Movie Categories' : 'Categories'} items={data} loading={loading} />
-            <ModeSwitch />
         </Flex>
     );
 };
