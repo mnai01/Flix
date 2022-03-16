e09ed1de598572c77505446b7c8648931b46ceb1
Ian M <44310933+mnai01@users.noreply.github.com>
2022-03-15 20:53:52 -0400

    Add watchedMedia Entity

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,4 +1,4 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Field, Int, ObjectType } from 'type-graphql';
 
 // BaseEntity gives us more functionality like User.save
@@ -20,4 +20,32 @@ export class User extends BaseEntity {
     @Field()
     @Column('int', { default: 1 })
     tokenVersion: number;
+
+    @Field(() => [WatchedMovies], { nullable: true })
+    @OneToMany(() => WatchedMovies, (movie) => movie.user, { cascade: true })
+    watchedMovies: WatchedMovies[];
+}
+
+// BaseEntity gives us more functionality like User.save
+@ObjectType()
+@Entity('watchedMedia')
+export class WatchedMovies extends BaseEntity {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => User, (user) => user.watchedMovies, { nullable: false })
+    user: User;
+
+    // it can infer string but not number so we need to specify
+    @Column('text')
+    @Field()
+    tmdb: string;
+
+    @Field()
+    @Column('text')
+    type: string;
+
+    @CreateDateColumn()
+    created_at: Date;
 }
