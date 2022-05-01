81f2bf79348c6d0fd5c3752ce507f641f019358e
Ian M <44310933+mnai01@users.noreply.github.com>
2022-05-01 15:39:24 -0400

    Update lower case emails only

--- server/src/resolvers/UserResolver.ts
+++ server/src/resolvers/UserResolver.ts
@@ -81,7 +81,7 @@ export class UserResolver {
     // Ctx stand for context. this is reference to the context information being passed in. Gives us access to the context
     async Login(@Arg('email') email: string, @Arg('password') password: string, @Ctx() { res }: MyContext): Promise<LoginResponse> {
         // Get user
-        const user = await User.findOne({ where: { email } });
+        const user = await User.findOne({ where: { email: email.toLowerCase() } });
         // Check if user exists
         if (!user) {
             throw new Error('could not find user');
@@ -202,7 +202,7 @@ export class UserResolver {
             }
 
             // Insert it into DB
-            await User.insert({ email, password: hashedPassword, role: payload.role, plainText: payload.plainText });
+            await User.insert({ email: email.toLowerCase(), password: hashedPassword, role: payload.role, plainText: payload.plainText });
             return true;
         } catch (err) {
             return err;
