ffde9f249f28e64616d8e2f942afd45880e9bb5b
Ian M <44310933+mnai01@users.noreply.github.com>
2022-05-13 09:17:56 +0800

    Add expiration on cookie

--- server/src/helpers/sendTokens.ts
+++ server/src/helpers/sendTokens.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
 
 export const sendRefreshToken = (res: Response, token: string) => {
-    res.cookie('jid', token, { httpOnly: true });
+    res.cookie('jid', token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
 };
