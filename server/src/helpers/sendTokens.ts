b484b211af46c4150c38db55a5ad012b989d0e2c
Ian M <44310933+mnai01@users.noreply.github.com>
2022-10-05 01:12:08 -0400

    httpOnly comment

--- server/src/helpers/sendTokens.ts
+++ server/src/helpers/sendTokens.ts
@@ -1,5 +1,8 @@
 import { Response } from 'express';
 
 export const sendRefreshToken = (res: Response, token: string) => {
+    // httpOnly makes it so cookies cant be access via javascript
+    // We still add it to the browser and it will get sent to every request (automatically, thats how fetch operates)
+    // but it cannot be tampered with this setting
     res.cookie('jid', token, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
 };
